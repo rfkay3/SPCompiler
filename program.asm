6e245f0786cc65d73aee253df61084001c7f08a0
Chris Paxton <paxtoncr@miamioh.edu>
2019-10-07 16:42:46 -0400

    Actually fixed merge conflict and added character functionality

--- program.asm
+++ program.asm
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
 declare a, integer
 store 2, a
 declare b, integer
 declare c, integer
 declare d, integer
 store 123234, d
-=======
->>>>>>> 15fbac1bfcbac98e31e00ac7e6390ac1ef8e42c5
 declare x, real
 declare y, real
 store 2.2, y
 declare z, real
 store 1.0, z
-<<<<<<< HEAD
 declare i, character
 store '1', i
 declare j, character
@@ -23,6 +19,9 @@ store '-', l
 declare m, character
 store '.', m
 declare n, character
-=======
->>>>>>> 15fbac1bfcbac98e31e00ac7e6390ac1ef8e42c5
+declare x, real
+declare y, real
+store 2.2, y
+declare z, real
+store 1.0, z
 halt
--- program.pas
+++ program.pas
@@ -1,12 +1,9 @@
 program
 var
-<<<<<<< HEAD
 integer a:=2, b, c, d:=123234;
 real x, y:=2.2, z:=1.0;
 character i:='1', j, k:='f', l:='-', m:='.', n;
-=======
 real x, y:=2.2, z:=1.0;
->>>>>>> 15fbac1bfcbac98e31e00ac7e6390ac1ef8e42c5
 begin
 ;
 end.
