c962bd2da6459be95cc1727dfb8af019e8e458ec
kayrf <kayrf@ceclnx01-19.csi.miamioh.edu>
2019-10-28 18:29:41 -0400

    Add new example program

--- program.pas
+++ program.pas
@@ -3,8 +3,8 @@ var
 integer a:=2, b, c, d:=123234;
 real x, y:=2.2, z:=1.0;
 character i:='1', j, k:='f', l:='-', m:='.', n;
-real x, y:=2.2, z:=1.0;
 string p:="this is a string";
+// This is a comment.
 begin
 ;
 end.
