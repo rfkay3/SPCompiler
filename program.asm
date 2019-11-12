08cb8255b15add804336bcd80adb6c8d9478ba65
ulmeslj <ulmeslj@ceclnx01-19.csi.miamioh.edu>
2019-11-12 10:41:58 -0500

    new output

--- program.asm
+++ program.asm
@@ -19,8 +19,17 @@ store '-', l
 declare m, character
 store '.', m
 declare n, character
-declare x, real
-declare y, real
-store 2.2, y
-declare z, real
-store 1.0, z
+declare p, string
+store "this is a string", p
+declare &tempi1, integer
+imult a, a, &tempi1
+declare &tempi2, integer
+iadd d, d, &tempi2
+declare &tempi3, integer
+iadd &tempi1, &tempi2, &tempi3
+declare &tempi4, integer
+store &tempi3, &tempi4
+declare &tempr1, real
+itor &tempi4, &tempr1
+store &tempr1, x
+halt
